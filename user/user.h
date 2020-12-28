43cf06fc6775ad0fb1a8adc72f3a60ab993a0507
Dys <1608003267@qq.com>
2020-12-29 00:48:13 +0800

    mmap

--- user/user.h
+++ user/user.h
@@ -28,6 +28,8 @@ int crash(const char*, int);
 int mount(char*, char *);
 int umount(char*);
 
+void *mmap(void *,int ,int ,int ,int ,int);
+int munmap(void *,int);
 // ulib.c
 int stat(const char*, struct stat*);
 char* strcpy(char*, const char*);
